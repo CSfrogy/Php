09ef1ff2d8cdde37c49e510aa3feb68857e21b62
CSfrogy <134266389+CSfrogy@users.noreply.github.com>
2025-02-16 16:07:46 +0800

    add controllers
    Add controllers and link them fix main "index.php" file
    
    P.S.I fixed problem when I start server project from VSCODE it has to open "local:3000" not "local:3000/index.php"
    I manually click in terminal on server and then open it again.

--- functions.php
+++ functions.php
@@ -1,17 +1,5 @@
-<?php 
-function dd($value)
-{
-    echo "<pre>";
-    var_dump($value);
-    echo "</pre>";
-
-    die();
-}
-
+<?php
 function urlIs($value)
 {
     return $_SERVER['REQUEST_URI'] == $value;
-}
-
-
-;?>
+};
