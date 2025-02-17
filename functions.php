6763516aec8da4c83c573881abb8663fab511658
CSfrogy <134266389+CSfrogy@users.noreply.github.com>
2025-02-17 10:55:19 +0800

    made PHP Router
    Created PHP router file for better access and navigation through our project,also included 404 php if user trying access non exist page in url

--- functions.php
+++ functions.php
@@ -1,4 +1,14 @@
 <?php
+function dd($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    '</pre>';
+    die();
+}
+
+
+
 function urlIs($value)
 {
     return $_SERVER['REQUEST_URI'] == $value;
--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<?php 
+<?php
 
 require 'functions.php';
+require 'router.php';
 
-
-$uri = $_SERVER['REQUEST_URI'];
-if ($uri === '/'){
-    require 'controllers/index.php';
-}else if($uri === '/about'){
-    require 'controllers/about.php';
-}else if ($uri ==='/contact'){
-    require 'controllers/contact.php';
-}
