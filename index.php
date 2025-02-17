50f02164c9a120a73ebd075436dd0b20f98be4b7
CSfrogy <134266389+CSfrogy@users.noreply.github.com>
2025-02-17 13:31:58 +0800

    connect our DB(PDO)
    connection string $dsn("...");

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
 
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+
+
+$pdo = new PDO($dsn,'root','123');
+
+
+$statement = $pdo -> prepare("select * from posts");
+$statement->execute();
+
+
+$posts = $statement->fetchAll();
+
+
+dd($posts);
