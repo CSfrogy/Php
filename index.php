f6b17512d70477b8474f0e23839a7127cd6171ea
CSfrogy <134266389+CSfrogy@users.noreply.github.com>
2025-02-17 14:22:05 +0800

    Database connection
    connection to our database where we execute our function to check all post or specific post

--- index.php
+++ index.php
@@ -2,18 +2,11 @@
 
 require 'functions.php';
 // require 'router.php';
-
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
-
-
-$pdo = new PDO($dsn,'root','123');
+require 'Database.php';
 
 
-$statement = $pdo -> prepare("select * from posts");
-$statement->execute();
 
-
-$posts = $statement->fetchAll();
-
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
 dd($posts);
