6fa0096a5a9da863c5001524a32969ae826d4393
CSfrogy <134266389+CSfrogy@users.noreply.github.com>
2025-02-25 23:56:13 +0800

    routes created
    note-create file,post method,build a small form

--- router.php
+++ router.php
@@ -1,14 +1,6 @@
 <?php
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$routes = require('routes.php');
 
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/contact' => 'controllers/contact.php',
-];
 
 function routeToController($uri, $routes)
 {
@@ -29,5 +21,6 @@ function abort($code = 404)
     die();
 }
 
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($uri, $routes);
--- views/notes.view.php
+++ views/notes.view.php
@@ -3,13 +3,19 @@
 <?php require('partials/banner.php'); ?>
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <?php foreach ($notes as $note) : ?>
-      <li>
-        <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
-          <?= $note['body'] ?>
-        </a>
-      </li>
-    <?php endforeach; ?>
+    <ul>
+      <?php foreach ($notes as $note) : ?>
+        <li>
+          <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
+            <?= $note['body'] ?>
+          </a>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+
+    <p class="mt-10">
+      <a href="/notes/create"class="text-blue-500 hover:undeline">Create note</a>
+    </p>
   </div>
 </main>
 <?php require('partials/footer.php'); ?>
