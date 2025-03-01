a7aae1e7a135ab513135d241f38e9af71ff9ca55
CSfrogy <134266389+CSfrogy@users.noreply.github.com>
2025-03-01 15:31:07 +0800

    Building better php router
    in process of rebuild php router

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -1,6 +1,6 @@
-<?php require base_path ('views/partials/head.php'); ?>
-<?php require base_path ('views/partials/nav.php'); ?>
-<?php require base_path ('views/partials/banner.php'); ?>
+<?php require base_path('views/partials/head.php'); ?>
+<?php require base_path('views/partials/nav.php'); ?>
+<?php require base_path('views/partials/banner.php'); ?>
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
     <p class="mb-6">
@@ -9,6 +9,11 @@
     <p>
       <?= htmlspecialchars($note['body']) ?>
     </p>
+
+    <form class="mt-6" method="POST">
+      <input name="id" type="text" value="<?= $note['id'] ?>">
+      <button type="submit" class="text-red-500">Delete Note</button>
+    </form>
   </div>
 </main>
 
