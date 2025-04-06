3127e923b8a91a023755665fc21587e26f644a0e
CSfrogy <134266389+CSfrogy@users.noreply.github.com>
2025-04-06 23:32:22 +0800

    ???

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -16,11 +16,14 @@ $form = new LoginForm();
 if ($form->validate($email, $password)) {
     if ((new Authenticator)->attempt($email, $password)) {
         redirect('/');
-    } 
+    }
     $form->error('email', 'No matching account found for that email address and password.');
-    
 }
 
-Session::flash('errors',$form->errors());
+Session::flash('errors', $form->errors());
+
+Session::flash('old',[
+    'email' =>$_POST['email']
+]);
 
 return redirect('/login');
