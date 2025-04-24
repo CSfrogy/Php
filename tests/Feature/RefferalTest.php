c08eedf1280dd52beb4543375bab0619efe391dc
CSfrogy <134266389+CSfrogy@users.noreply.github.com>
2025-04-24 13:00:12 +0800

    last changes

---
+++ tests/Feature/RefferalTest.php
@@ -0,0 +1,11 @@
+<?php
+
+it('allows subcribers to earn money by reffering their friends', function () {
+    // Refferal
+    // redeem
+});
+
+
+it('disallows guest from participating in the reffereal program',function(){
+
+});
